660df0001d476ec57a756804f91cdb9390397e42
kingdayx <54965488+kingdayx@users.noreply.github.com>
2020-02-03 14:16:10 -0500

    switched styles
    switch from inline styles to a stylesheet

--- App.js
+++ App.js
@@ -1,11 +1,11 @@
 import React from 'react';
-import { StyleSheet, Text, View, Button , TextInput } from 'react-native';
+import { StyleSheet, Text, View, Button, TextInput } from 'react-native';
 
 export default function App() {
   return (
-    <View style={{padding:50}}>
-      <View>
-        <TextInput placeholder="Course Goal" style={{bordermColor: "black", borderWidth: 1, padding:10}} />
+    <View style={styles.screen}>
+      <View style={styles.inputContainer} >
+        <TextInput placeholder="Course Goal" style={styles.input} />
         <Button title="ADD" />
       </View>
       <View>
@@ -15,3 +15,10 @@ export default function App() {
   );
 }
 
+const styles = StyleSheet.create({
+  screen: {
+    padding: 50
+  },
+  inputContainer: { flexDirection: "row", justifyContent: "space-between", alignItems: "center" },
+  input:{ width: 200, borderColor: "black", borderWidth: 1, padding: 10 }
+});
