a4e04433bf07a0d18952323e4f2de0231ea24b4f
kingdayx <54965488+kingdayx@users.noreply.github.com>
2020-02-03 18:49:25 -0500

    added map funtion and styles
    added map function to iterate over textInput data to place under view of text input and button, adding texts with new added styles

--- App.js
+++ App.js
@@ -3,13 +3,14 @@ import { StyleSheet, Text, View, Button, TextInput } from 'react-native';
 
 export default function App() {
   const [enteredGoal, setEnteredGoal] = useState('');
+  const [courseGoals, setCourseGoals] = useState([]);
 
   const goalinputHandler = (enteredText)=> {
     setEnteredGoal(enteredText)
   };
 
   const addGoalHandler = ()=>{
-    console.log(enteredGoal)
+    setCourseGoals(currentGoals => [...courseGoals, enteredGoal])
   };
 
   return (
@@ -21,7 +22,7 @@ export default function App() {
         <Button title="ADD" onPress={addGoalHandler} />
       </View>
       <View>
-
+        {courseGoals.map((goal)=> <View style={styles.listItem} ><Text key={goal} >{goal} </Text></View> )}
       </View>
     </View>
   );
@@ -32,5 +33,11 @@ const styles = StyleSheet.create({
     padding: 50
   },
   inputContainer: { flexDirection: "row", justifyContent: "space-between", alignItems: "center" },
-  input:{ width: 200, borderColor: "black", borderWidth: 1, padding: 10 }
+  input:{ width: 200, borderColor: "black", borderWidth: 1, padding: 10 },
+  listItem:{
+    padding:10,
+    backgroundColor:'#ccc',
+    borderColor:"black",
+    borderWidth:1
+  }
 });
